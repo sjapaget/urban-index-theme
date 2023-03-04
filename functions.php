38eb481a87808d0af1dd8e03dfcb0107b47016ed
Samuel Paget <sjapaget@gmail.com>
2023-03-04 12:24:40 +0100

    Add latest articles block pattern

--- functions.php
+++ functions.php
@@ -29,10 +29,33 @@ function urban_index_block_patterns() {
             'title'       => __( 'Latest Articles in Category', 'urban-index'),
             'description' => _x( 'Use to display the latest articles in a category', 'urban-index' ),
             'categories'  => array( 'urban-index', 'query' ),
-            'content'     => ''
+            'content'     => '<!-- wp:group {"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group"><!-- wp:heading -->
+			<h2>Latest Articles</h2>
+			<!-- /wp:heading -->
+			
+			<!-- wp:query {"queryId":6,"query":{"perPage":"6","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[5]}},"displayLayout":{"type":"flex","columns":3}} -->
+			<div class="wp-block-query"><!-- wp:post-template -->
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}},"border":{"radius":"1rem"}},"borderColor":"main","className":"is-style-full-height","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group is-style-full-height has-border-color has-main-border-color" style="border-radius:1rem;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:post-featured-image {"height":""} /-->
+			
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"className":"is-style-default","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group is-style-default" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-date /-->
+			
+			<!-- wp:post-title /--></div>
+			<!-- /wp:group --></div>
+			<!-- /wp:group -->
+			<!-- /wp:post-template -->
+			
+			<!-- wp:query-no-results -->
+			<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+			<p></p>
+			<!-- /wp:paragraph -->
+			<!-- /wp:query-no-results --></div>
+			<!-- /wp:query --></div>
+			<!-- /wp:group -->'
 			)
     );
-
 
     register_block_pattern(
         'urban-index/ui-category-all-articles-query',
